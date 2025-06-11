1b3fe74ea56bdd16c17331d568ab93c9279a19d4
Gabriel Enzo <111479378+GabrielEnzoVidaldeAlmeida@users.noreply.github.com>
2025-06-11 12:06:08 -0300

    terminando estilização da navbar

--- frontend/src/components/NavBar/index.tsx
+++ frontend/src/components/NavBar/index.tsx
@@ -1,17 +1,41 @@
 "use client";
 
 import { useAuth } from "@/contexts/auth-context";
+import { LogOutIcon, PlusIcon } from "lucide-react";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
+import { useState, useRef, useEffect } from "react";
 
 export default function NavBar() {
   const { user, logout, loading } = useAuth();
   const router = useRouter();
+  const [handleProfile, setHandleProfile] = useState(false);
+  const menuRef = useRef<HTMLDivElement>(null);
+
+  const toggleHandleProfile = () => {
+    setHandleProfile((prev) => !prev);
+  };
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
+        setHandleProfile(false);
+      }
+    };
+
+    if (handleProfile) {
+      document.addEventListener("mousedown", handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [handleProfile]);
 
   if (loading) return null;
 
   return (
-    <nav className="bg-black flex justify-between items-center p-4 sm:p-6 md:p-8">
+    <nav className="bg-black flex justify-between items-center p-4 sm:p-6 md:p-8 relative">
       <Link href="/">
         <h1 className="text-slate-100 text-xl font-extrabold sm:text-2xl md:text-3xl hover:brightness-75 transition">
           The Noticiary
@@ -19,18 +43,37 @@ export default function NavBar() {
       </Link>
 
       {user ? (
-        <div className="text-slate-100 font-extrabold flex items-center gap-1 hover:brightness-75 transition">
-          <span className="text-sm sm:text-base md:text-xl">
-            Olá, {user.username}
-          </span>
+        <div className="relative">
           <button
-            onClick={() => {
-              logout();
-              router.push("/login");
-            }}
+            onClick={toggleHandleProfile}
+            className="text-slate-100 font-extrabold flex items-center gap-1 text-sm sm:text-base md:text-xl hover:brightness-75 transition"
           >
-            Sair
+            Olá, {user.username}
           </button>
+
+          {handleProfile && (
+            <div
+              ref={menuRef}
+              className="absolute right-0 mt-2 w-48 bg-white rounded-md shadow-lg z-50 p-4 space-y-3 text-black"
+            >
+              <Link
+                href="#"
+                className="flex items-center gap-2 hover:text-blue-600 transition"
+              >
+                <PlusIcon size={18} /> Adicionar notícia
+              </Link>
+
+              <button
+                onClick={() => {
+                  logout();
+                  router.push("/login");
+                }}
+                className="flex items-center cursor-pointer gap-2 text-red-600 hover:text-red-800 transition"
+              >
+                <LogOutIcon size={18} /> Sair
+              </button>
+            </div>
+          )}
         </div>
       ) : (
         <div className="flex gap-8">
